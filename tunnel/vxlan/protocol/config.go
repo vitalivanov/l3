3b2d33674904873cdc046f32b4e512d159e140a1
ccordes-snaproute <ccordes@snaproute.com>
2016-05-18 19:39:01 -0700

    fixed revert change

--- tunnel/vxlan/protocol/config.go
+++ tunnel/vxlan/protocol/config.go
@@ -210,7 +210,7 @@ func ConvertVxlanVtepInstanceToVtepConfig(c *vxland.VxlanVtepInstance) (*VtepCon
 		UDP:       uint16(c.DstUDP),
 		TTL:       uint16(c.TTL),
 		TOS:       uint16(c.TOS),
-		InnerVlanHandlingMode: c.InnerVlanHandlingMode,
+		InnerVlanHandlingMode: ConvertInt32ToBool(c.InnerVlanHandlingMode),
 		TunnelSrcIp:           ip,
 		TunnelDstIp:           net.ParseIP(c.DstIp),
 		VlanId:                uint16(c.VlanId),
